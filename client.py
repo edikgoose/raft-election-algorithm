05fd7fe91d9f23ee82011a88f3d4b1001f61db56
Iskander Nafikov <nafikov.iskander@mail.ru>
2022-11-14 22:21:05 +0300

    Add client

---
+++ client.py
@@ -0,0 +1,91 @@
+import grpc
+import raft_pb2
+import sys
+
+from raft_pb2_grpc import RaftElectionServiceStub as RaftElectionService
+from typing import Optional
+
+AppendRequest = raft_pb2.AppendRequest
+AppendResponse = raft_pb2.AppendResponse
+GetLeaderResponse = raft_pb2.GetLeaderResponse
+SuspendRequest = raft_pb2.SuspendRequest
+Void = raft_pb2.Void
+VoteRequest = raft_pb2.VoteRequest
+VoteResponse = raft_pb2.VoteResponse
+
+
+class CommandNotExistError(Exception):
+    pass
+
+
+class NoServerProvidedError(Exception):
+    pass
+
+
+class UserService:
+
+    def __init__(self):
+        self.address: Optional[str] = None
+        self.service: Optional[RaftElectionService] = None
+
+    def connect(self, address: str) -> None:
+        self.address = address
+        channel = grpc.insecure_channel(address)
+        self.service = RaftElectionService(channel)
+
+    def get_leader(self) -> (int, str):
+        self.__validate_server()
+        request = Void()
+        response: GetLeaderResponse = self.service.GetLeader(request)
+        return response.nodeId, response.nodeAddress
+
+    def suspend(self, period_sec: int) -> None:
+        self.__validate_period(period_sec)
+        self.__validate_server()
+        request = SuspendRequest(period_sec)
+        self.service.Suspend(request)
+
+    def __validate_server(self):
+        if not self.service:
+            raise NoServerProvidedError("No server address provided")
+
+    @staticmethod
+    def __validate_period(period: int):
+        if period is not int or (0 < period > 3600):
+            raise ValueError("Period must an integer that belongs to range [0, 3600]")
+
+
+def main() -> None:
+    service = UserService()
+    print("The client starts")
+
+    while True:
+        try:
+            line = input('> ')
+            if not line:
+                continue
+
+            command, *args = line.split(maxsplit=1)
+
+            if command == "connect":
+                service.connect(args[0])
+            elif command == "getleader":
+                response = service.get_leader()
+                print(*response)
+            elif command == "suspend":
+                service.suspend(int(args[0]))
+            elif command == "quit":
+                raise KeyboardInterrupt
+            else:
+                raise CommandNotExistError("Command does not exist")
+
+        except KeyboardInterrupt:
+            print("The client ends")
+            sys.exit(0)
+        except Exception as e:
+            print(e)
+            print("Try again!")
+
+
+if __name__ == "__main__":
+    main()
